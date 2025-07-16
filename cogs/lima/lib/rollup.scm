70dd1763b3de013e9f73a91254558480e4bc358d
Simon Guest <simon.guest@tesujimath.org>
2025-07-16 14:50:37 +1200

    Combined rollup progress

--- cogs/lima/lib/rollup.scm
+++ cogs/lima/lib/rollup.scm
@@ -1,5 +1,6 @@
 (provide display-rollup)
 
+(require "steel/sorting/merge-sort.scm")
 (require "srfi/srfi-28/format.scm")
 (require "lima/lib/types.scm")
 (require "lima/lib/alist.scm")
@@ -15,10 +16,12 @@
          ldg
          #:cur
          [cur (ledger-main-currency ldg)])
-  (let* ((depth-rollup (foldl (lambda (accname depth-rollup)
+  (let* ((account-names (ledger-account-names ldg))
+         (accounts (ledger-accounts ldg))
+         (depth-rollup (foldl (lambda (accname depth-rollup)
                                 (let* ((depth (car depth-rollup))
                                        (rollup0 (cdr depth-rollup))
-                                       (acc (hash-get (ledger-accounts ldg) accname))
+                                       (acc (hash-get accounts accname))
                                        (bal (alist-get-or-default cur (decimal-zero) (account-inventory acc)))
                                        (subacns (split-many accname ":"))
                                        (parent-accnames (foldl (lambda (sub accs)
@@ -35,8 +38,23 @@
                                                        parent-accnames)))
                                   (cons (max depth (length subacns)) rollup1)))
                         (cons 0 (hash))
-                        (ledger-account-names ldg))))
-    depth-rollup))
+                              account-names))
+         (depth (car depth-rollup))
+         (rollup (transduce (cdr depth-rollup)
+                            (filtering (lambda (name-bal) (not (decimal-zero? (second name-bal)))))
+                            (into-hashmap)))
+         (rollup-account-names (merge-sort (transduce rollup
+                                                      (mapping first)
+                                                      (into-list))
+                                           #:comparator string<?))
+         (rollup-combined (transduce rollup-account-names
+                                     (mapping (lambda (accname) (list accname
+                                                                      (hash-get rollup accname)
+                                                                      (let ((acc (hash-try-get accounts accname)))
+                                                                        (if acc (alist-get-or-default cur (decimal-zero) (account-inventory acc))
+                                                                            (decimal-zero))))))
+                                     (into-list))))
+    rollup-combined))
 
 (define (display-rollup ldg)
   (display (tabulate (collate-rollup ldg) 'left 'centre)))
