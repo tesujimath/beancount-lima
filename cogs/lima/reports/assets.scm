af41f01e0651cc7f01bb42f83bf90b362b4a240c
Simon Guest <simon.guest@tesujimath.org>
2025-10-23 09:51:11 +1300

    Fix assets report for new-style filters

--- cogs/lima/reports/assets.scm
+++ cogs/lima/reports/assets.scm
@@ -2,7 +2,7 @@
 
 (let* ((rollup (hash-try-get *args* 'rollup))
        (assets-account-name (hash-get *options* 'name_assets))
-       (cum (cumulate-postings *directives* #:filters (hash 'account (lambda (acc) (starts-with? acc assets-account-name))))))
+       (cum (cumulate-postings *directives* (f/subacc assets-account-name))))
   (if rollup
       (display-rollup cum)
       (display-balance-sheet cum)))
